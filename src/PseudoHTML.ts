1058730c49d680eb651f504519a678078b7f97c6
Olexandr Pascal <futupas@gmail.com>
2021-06-02 12:11:37 +0300

    parsing attributes

--- src/PseudoHTML.ts
+++ src/PseudoHTML.ts
@@ -10,180 +10,6 @@ export class PseudoHTML {
         public startingInString?: number,
     ) { }
 
-    public parse2(): IParsedPseudoHTML {
-        /*
-        input: "span.class1.class2#id1[hello=world num=8 countries='Kosovo DNR USSR' autocomplete]{some text $@3}*15"
-        out: {
-            raw: "span.class1.class$#id1[hello=world num=8 countries='Kosovo DNR USSR' autocomplete]{some text $@3}*15",
-            tagName: 'span',
-            id: 'id1',
-            classList: ['class1', 'class$'],
-            attributes: [{
-                name: 'hello', value: 'world'
-            }, {
-                name: 'num', value: '8'
-            }, {
-                name: 'countries', value: 'Kosovo DNR USSR'
-            }, {
-                name: 'autocomplete'
-            }],
-            innerText: 'some text $@3',
-            quantity: 15,
-        }
-        useful docs: https://docs.emmet.io/cheat-sheet/
-        */
-        const enum SpecialChars {
-            Tag = '',
-            Class = '.',
-            Id = '#',
-            Attrib = '[',
-            EndAttrib = ']',
-            Equals = '=',
-            InnerText = '{',
-            EndInnerText = '}',
-            Mult = '*'
-        }
-
-        const specialChars = ['', '#', '.', '[', '=', ']', '{', '}', '*', ' '];
-
-
-        const result: IParsedPseudoHTML = {
-            raw: this.raw,
-            attributes: [],
-            classList: [],
-        };
-
-        let prevState = '';
-        let currentText: string = '';
-        let write: boolean = true;
-
-        let multiplierNow = false;
-
-        const varRe = /([a-zA-Z]){1}([a-zA-Z]|-|_|[0-9])*/;
-        const tagRe = /(([a-zA-Z]){1}([a-zA-Z]|-|_|[0-9])*)*/;
-
-        for (let i = 0; i < this.raw.length+1; i++) {
-            if (prevState === SpecialChars.EndAttrib || prevState === SpecialChars.EndInnerText) write = false;
-            let endOfLine : boolean = false;
-            if(i==this.raw.length) {i-=1; endOfLine=true;}
-            if (specialChars.includes(this.raw[i]) || endOfLine) {
-
-                if (prevState === SpecialChars.InnerText && this.raw[i] === SpecialChars.EndInnerText) {
-                    if (write) result.innerText = currentText;
-                    prevState = this.raw[i];
-                    currentText = '';
-                }
-
-                if (prevState === SpecialChars.Attrib && this.raw[i] === SpecialChars.EndAttrib) {
-                    if (write) result.attributes.push({ name: currentText });
-                    prevState = this.raw[i];
-                    currentText = '';
-                }
-                // Check previous special char if we found a new one
-                switch (prevState) {
-                    case SpecialChars.InnerText:
-                        break;
-                    case SpecialChars.Attrib:
-                        break;
-                    case SpecialChars.Tag:
-                        if (write) {
-                            result.tagName = currentText;
-                            if (!currentText.match(tagRe).includes(currentText))
-                                throw new EmmetStringParsingError('tag name syntax error', i);
-                        }
-                        prevState = this.raw[i];
-                        currentText = '';
-                        break;
-                    case SpecialChars.Id:
-                        if (write) {
-                            result.id = currentText;
-                            if (!currentText.match(varRe).includes(currentText))
-                                throw new EmmetStringParsingError('id name syntax error', i);
-                        }
-                        prevState = this.raw[i];
-                        currentText = '';
-                        break;
-                    case SpecialChars.Class:
-                        if (write) {
-                            result.classList.push(currentText);
-                            if (!currentText.match(varRe).includes(currentText))
-                                throw new EmmetStringParsingError('class name syntax error', i);
-                        }
-                        prevState = this.raw[i];
-                        currentText = '';
-                        break;
-                    case SpecialChars.EndAttrib:
-                        if (write) {
-
-                            let isInQuotes = false;
-                            let quoteChar = '';
-                            let quoteStart = true;
-
-                            let isName = true;
-
-                            let name = '';
-                            let value = '';
-                            for (let j = 0; j < currentText.length; j++) {
-                                if (currentText[j] === ' ') {
-                                    if (isName) isName = false;
-                                    if (isInQuotes) value += currentText[j];
-                                    if (!isInQuotes && name.length !== 0 && value.length !== 0) {
-                                        if (!name.match(varRe).includes(currentText))
-                                            throw new EmmetStringParsingError("attribute name syntax error", i);
-                                        result.attributes.push({name, value});
-                                    }
-                                } else if (currentText[j] === '=') {
-                                    if(!isInQuotes) {
-                                        isName=false;
-                                    }
-                                } else if(currentText[j] === '\"') {
-                                    if (!quoteStart) {
-                                        quoteStart = true;
-                                        quoteChar = '\"';
-                                    }
-                                    if (quoteStart){
-                                        if (currentText[j-1] !== '\\') quoteStart = false;
-                                    }
-                                } else if (currentText[j] === '\'') {
-                                    if (!quoteStart) {
-                                        quoteStart = true;
-                                        quoteChar = '\'';
-                                    }
-                                    if (quoteStart){
-                                        if (currentText[j-1] !== '\\') quoteStart = false;
-                                    }
-                                } else if (isName) {
-                                    name += currentText[j];
-                                } else if (!isName) {
-                                    value += currentText[j];
-                                }
-                            }
-                            if (!isName)
-                                throw new EmmetStringParsingError('missing value after =', i);
-
-                        }
-                        prevState = this.raw[i];
-                        currentText = '';
-                        break;
-                    case SpecialChars.EndInnerText:
-                        if (write) result.innerText = currentText;
-                        prevState = this.raw[i];
-                        currentText = '';
-                        break;
-                    case SpecialChars.Mult:
-                        if (write) result.quantity = parseInt(currentText);
-                        prevState = this.raw[i];
-                        currentText = '';
-                        multiplierNow = true;
-                        break;
-                }
-            } else {
-                currentText += this.raw[i];
-            }
-        }
-
-        return result;
-    }
     public parse(): IParsedPseudoHTML {
         /*
         input: "span.class1.class2#id1[hello=world num=8 countries='Kosovo DNR USSR' autocomplete]{some text $@3}*15"
@@ -223,13 +49,20 @@ export class PseudoHTML {
             id,
             innerText,
             quantity,
+            attrName,
+            attrValue,
         }
+        const enum AttrValueType {
+            simple,
+            oneQuote,
+            twoQuotes
+        }
+        let currentAttrValue: AttrValueType = undefined;
         let currentParsing = ParsedType.tag;
 
         for (let i = 0; i < raw.length; i++) {
             const c = raw[i];
-            // Todo: support '\{'
-            //todo: type attr
+            // Todo: support '\{', '\"'
 
             if (currentParsing === ParsedType.innerText) {
                 if (c === '}') {
@@ -238,6 +71,41 @@ export class PseudoHTML {
                     result.innerText += c;
                     continue;
                 }
+            }
+            if (currentParsing === ParsedType.attrValue) {
+                if (c === ']') {
+                    currentParsing = undefined;
+                    continue;
+                }
+                if (c === ' ' && currentAttrValue !== AttrValueType.oneQuote && currentAttrValue !== AttrValueType.twoQuotes) {
+                    currentAttrValue = undefined;
+                    currentParsing = ParsedType.attrName;
+                    result.attributes.push({ name: '', value: '' });
+                    continue;
+                }
+                if (!currentAttrValue) {
+                    if (c === '\'') {
+                        currentAttrValue = AttrValueType.oneQuote;
+                    } else if (c === '\"') {
+                        currentAttrValue = AttrValueType.twoQuotes;
+                    } else if (c === ' ') {
+                        // Do nothing
+                    } else {
+                        currentAttrValue = AttrValueType.simple;
+                        result.attributes[result.attributes.length-1].value += c;
+                    }
+                    continue;
+                }
+                if ((currentAttrValue === AttrValueType.oneQuote &&  c === '\'') ||
+                    (currentAttrValue === AttrValueType.twoQuotes &&  c === '\"')) {
+                        currentAttrValue = undefined;
+                        currentParsing = ParsedType.attrName;
+                        result.attributes.push({ name: '', value: '' });
+                        continue;
+                }
+
+                result.attributes[result.attributes.length-1].value += c;
+                continue;
             }
 
             switch(c) {
@@ -250,6 +118,15 @@ export class PseudoHTML {
                     break;
                 case '{':
                     currentParsing = ParsedType.innerText;
+                    break;
+                case '[':
+                    result.attributes.push({ name: '', value: '' });
+                    currentParsing = ParsedType.attrName;
+                    continue;
+                    break;
+                case ']':
+                    currentParsing = undefined;
+                    continue;
                     break;
                 case '*':
                     currentParsing = ParsedType.quantity;
@@ -272,10 +149,37 @@ export class PseudoHTML {
                             const newNum = (result.quantity || 0) * 10 + (+c);
                             result.quantity = newNum;
                             break;
+                        case ParsedType.attrName:
+                            if (c === ']') {
+                                currentParsing = undefined;
+                                continue;
+                            }
+                            if (c === ' ' && result.attributes[result.attributes.length-1].name !== '') {
+                                // This attr has no value, next one
+                                result.attributes[result.attributes.length-1].value = undefined;
+                                result.attributes.push({ name: '', value: '' });
+                                continue;
+                            }
+                            if (c === '=') {
+                                currentParsing = ParsedType.attrValue;
+                                currentAttrValue = undefined;
+                                continue;
+                            }
+
+                            result.attributes[result.attributes.length-1].name += c;
+                            break;
                     }
             }
         }
-
+        result.attributes = result.attributes
+            .map(attr => {
+                const newName = attr.name.trim();
+                return {
+                    name: (newName && newName.length) ? newName : undefined,
+                    value: (attr.value && attr.value.length) ? attr.value : undefined
+                };
+            })
+            .filter(attr => attr.name);
         console.log(result);
         return result;
 
